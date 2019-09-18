ec7944c986e090a12766c3d51d9831f35e48cfe3
andreamcm <cor16076@uvg.edu.gt>
2019-09-18 16:41:20 -0600

    Avances2

--- sim_gorilla.py
+++ sim_gorilla.py
@@ -1,8 +1,18 @@
-##Simulacion de eventos para la empresa Gorilla
-## Una hora de ejecucion
+# ------------------------------------- #
+# Universidad del Valle de Guatemala    #
+# Autoras:                              #
+#   Michelle Bloomfield, 16803          #
+#   Andrea Cordón, 16076                #
+# sim_gorilla.py                        #
+# ------------------------------------- #
+
+# Simulacion de eventos para la empresa Gorilla
+# Una hora de ejecucion
 
 import random
 import math
+import sys
+import time
 
 
 ### Arrival: Lambda = 2400 por minuto 
@@ -10,41 +20,68 @@ import math
 def generador(s, x):
     return s - (1/x)* math.log(random.random())
 
-t = Na = Nd = 0
-ES = 0 
+# # Variables de tiempo
+# T = 0   # tiempo de cierre
+# Tp = 0  # tiempo extra realizado por el servidor
+# t = 0   # tiempo de la simulación
 
-to = generador(0, 2400)
+#     # Contadores
+# Na = 0  # numero de llegadas al tiempo t
+# Nd = 0  # numero de salidas al tiemop t
+    
+#     # Estado del sistema
+# n = 0   # numero de solicitudes en el sistema en el tiempo t 
 
-ta = to 
-td = 999999
-tp = 0
+#     # Listas con tiempos
+# A = []
+# D = []
 
-start_time = time.time()
+# Este servidor puede atender hasta 100 solicitudes por segundo
+def cases():
+    # Variables de tiempo
+    T = 0   # tiempo de cierre
+    Tp = 0  # tiempo extra realizado por el servidor
+    t = 0   # tiempo de la simulación
 
-A = []
-D = []
+        # Contadores
+    Na = 0  # numero de llegadas al tiempo t
+    Nd = 0  # numero de salidas al tiemop t
+        
+        # Estado del sistema
+    n = 0   # numero de solicitudes en el sistema en el tiempo t 
 
-def cases(ta, td, t):
+        # Listas con tiempos
+    A = []
+    D = []
+
+    # Se comienza la simulacion
+    t = Na = Nd = 0
+    t0 = generador(0, 2400)
+    ta = t0
+    td = float("inf")
+
     ### Caso 1
-    if(ta<=td  and ta <= t):
+    if( ta <= td  and ta <= T ):
         t = ta
-        Na = Na + 1
-        n = n + 1
-        tt = generador(tt, 6000)
-        ta = tt
-        if n == 1: 
-            y = generador(t, 6000) 
-        A.append(t)
+        Na += 1
+        n += 1
+        Tt = generador( tt, 6000 )
+        ta = Tt
+        if( n == 1 ): 
+            Y = generador( t, 6000 ) 
+            td = t + Y
+        A.append(Na)
 
     ## Caso 2 
-    if(td< ta and td <= t): 
+    if( td < ta and td <= T ): 
         t = td
-        n = n-1
-        if n == 0:
-            td = 999999
+        n -= 1
+        Nd += 1
+        if( n == 0 ):
+            td = float("inf")
         else:  
-            y = generador(t, 6000)
-        D.append(t)
+            Y = generador( t, 6000 )
+        D.append(Nd)
 
     ## Caso 3
     if(min(ta, td) > t and n > 0):
@@ -52,19 +89,15 @@ def cases(ta, td, t):
         n -= 1
         Nd += 1
         if(n > 0):
-            y = generador(t, 6000)
-            td = t + y
-        D.append(td)
+            Y = generador(t, 6000)
+            td = t + Y
+        D.append(Nd)
         
     ## Caso 4
-    if(min(ta, td) > t and n = 0):
-        Tp = max(time.time() - t, 0)
-        break
+    if(min(ta, td) > T and n == 0):
+        Tp = max(time.time() - T, 0)
 
-# Puede atender hasta 100 solicitudes por segundo
-def gorilla_server():
-    break
+    return Na, ta, td
 
-# Son 10 servidores
-def ants_server():
-    break
+cant_solicitudes, tiempo_arrivals, tiempo_departures = cases()
+print(cant_solicitudes)
