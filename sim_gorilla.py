12aa57084c461e8a0cfb0d1fc0b01de5cfcecda6
Michelle Bloomfield <blo16803@uvg.edu.gt>
2019-09-18 15:50:59 -0600

    Avances

--- sim_gorilla.py
+++ sim_gorilla.py
@@ -17,17 +17,54 @@ to = generador(0, 2400)
 
 ta = to 
 td = 999999
+tp = 0
+
+start_time = time.time()
 
 A = []
+D = []
 
-### Caso 1
-if ta<=td  and ta <= t:
-    t = ta
-    Na = Na + 1
-    n = n + 1
-    tt = generador(tt, 6000)
-    ta = tt
-    if n == 1: 
-        y = generador(t, 6000) 
-    A.append(t)
+def cases(ta, td, t):
+    ### Caso 1
+    if(ta<=td  and ta <= t):
+        t = ta
+        Na = Na + 1
+        n = n + 1
+        tt = generador(tt, 6000)
+        ta = tt
+        if n == 1: 
+            y = generador(t, 6000) 
+        A.append(t)
 
+    ## Caso 2 
+    if(td< ta and td <= t): 
+        t = td
+        n = n-1
+        if n == 0:
+            td = 999999
+        else:  
+            y = generador(t, 6000)
+        D.append(t)
+
+    ## Caso 3
+    if(min(ta, td) > t and n > 0):
+        t = td
+        n -= 1
+        Nd += 1
+        if(n > 0):
+            y = generador(t, 6000)
+            td = t + y
+        D.append(td)
+        
+    ## Caso 4
+    if(min(ta, td) > t and n = 0):
+        Tp = max(time.time() - t, 0)
+        break
+
+# Puede atender hasta 100 solicitudes por segundo
+def gorilla_server():
+    break
+
+# Son 10 servidores
+def ants_server():
+    break
